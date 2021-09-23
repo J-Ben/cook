59ef01524c874895b65b70d2f0b558da5f503cfa
baptistemchl <baptistemichel22@gmail.com>
2021-09-23 12:39:58 +0200

    ajout listes

--- app/Views/ingredient/ingredient_all.php
+++ app/Views/ingredient/ingredient_all.php
@@ -1,5 +1,30 @@
 <?= $this->include("/ingredient/templates/header") ?>
+<body>
 
+<div class="container">
+  <h2>INGREDIENTS Table</h2>
+  <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>nom</th>
+        <th>type</th>
+        <th>quantité</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>ID</td>
+        <td>LIEBELLE</td>
+        <td>ORIGINE</td>
+        <td>ANNEE</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
 
 
 <?= $this->include("/ingredient/templates/footer") ?>
--- app/Views/recette/recette_all.php
+++ app/Views/recette/recette_all.php
@@ -1,5 +1,30 @@
 <?= $this->include("/recette/templates/header") ?>
 
+<body>
 
+<div class="container">
+  <h2>INGREDIENTS Table</h2>
+  <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>libelle</th>
+        <th>origine</th>
+        <th>année</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>ID</td>
+        <td>LIEBELLE</td>
+        <td>ORIGINE</td>
+        <td>ANNEE</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
 
 <?= $this->include("/recette/templates/footer") ?>
